d372ba2976dcdadd8484153c659c53d9d7f4f990
pgrigorenko <pgrigorenko>
2005-03-12 11:48:28 +0000

    added calc() implementation

--- gearbox/Table.java
+++ gearbox/Table.java
@@ -10,4 +10,23 @@ class Table {
 		[x ->y], first, step, last -> result {calc};
 	}
 	@*/
+	boolean calc (Subtask stm, int f, int s, int l) {
+		System.out.println("f " + f + " s " + s + " l " + l);
+		for (int i = f; i < l ; i += s )
+		{
+			Object[] in = new Object[1];
+			in[0] = new Integer(i);
+			Object[] out = null;
+			try
+			{
+				out = stm.run(in);
+			}
+			catch (Exception e)
+			{
+			}
+			
+			System.out.println("Out " + ((Integer)out[0]).intValue());
+		}
+		return true;
+	}
 }
