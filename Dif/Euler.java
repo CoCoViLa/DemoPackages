f2552f0fae0f29f54bb059e13f11b049cb5ff5d4
andrex42 <andrex42>
2008-01-17 07:58:19 +0000

    Updated the package to fix graph drawing with optimization turned on. Added debug flag to the simulation engine for switching between verbose and terse output.

--- Dif/Euler.java
+++ Dif/Euler.java
@@ -5,6 +5,7 @@ public class Euler {
 		int time;
 		void done_print_initstate, done_print_finalstate;
 		String initstate_name, finalstate_name;
+		int debug;
 
 		initstate_name = "initstate";
 		finalstate_name = "finalstate";
@@ -13,42 +14,45 @@ public class Euler {
 		alias (double) state      = (*.state);	
 		alias (double) nextstate  = (*.nextstate);
 		alias (double) finalstate = (*.finalstate);
+		alias          draw       = (*.drawing_ready);
 
 		initstate_name, initstate -> done_print_initstate  {print_state};
-		[ state ->  nextstate], initstate, time -> finalstate {proc_run};
+		[ state ->  nextstate, draw ], initstate, time, debug -> finalstate {proc_run};
 		finalstate_name, finalstate -> done_print_finalstate  {print_state};
-	}@*/	
+	}@*/
 
-	public void print_state(String state_name, double[] st) {		
-		System.out.print(state_name + ":");
-		for (int i = 0; i < st.length;  i++ ){
-			System.out.print("\t" + st[i]);	
-		}
-		System.out.println();			
-	}
+    public void print_state(String state_name, double[] st) {
+        System.out.print(state_name + ":");
+        for (int i = 0; i < st.length; i++) {
+            System.out.print("\t" + st[i]);
+        }
+        System.out.println();
+    }
 
-	public double[] proc_run(Subtask st, double[] initst, int time) {
-		double[]  state, finalst;
+    public double[] proc_run(Subtask st, double[] initst, int time, int debug) {
+        double[] state, finalst;
 
-		int done = 1;
-		System.out.println("time value is: " + time);
+        if (debug != 0) {
+            System.out.println("time value is: " + time);
+        }
 
-		Object[] in = new Object[1];
-		in[0] = initst;
-		try {		 
-			for (int i = 1; i <= time;  i++ ){
-				state = (double[])in[0];
-//				System.out.println("current time: " + i);
-//			     	print_state ("    state", state);	
-				Object[] out = st.run(in);
-				in = out;
-			}		
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			done = 0;
-		}
-		finalst = (double[])in[0];	
-	        return finalst;		 	
-	}
+        Object[] in = new Object[] { initst };
+        Object[] out;
+        try {
+            for (int i = 1; i <= time; i++ ) {
+                if (debug != 0) {
+                    state = (double[]) in[0];
+                    System.out.println("current time: " + i);
+                    print_state ("    state", state);
+                }
+                out = st.run(in);
+                in[0] = out[0]; // discard the second element draw
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finalst = (double[]) in[0];
+        return finalst;
+    }
 }
