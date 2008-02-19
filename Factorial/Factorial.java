56de4faa3dbb5b117f821928ea53024f6f26f0d7
pgrigorenko <pgrigorenko>
2008-02-19 11:59:56 +0000

    Updated Factorial demo class

--- Factorial/Factorial.java
+++ Factorial/Factorial.java
@@ -1,27 +1,29 @@
 class Factorial {
+    
     /*@ specification Factorial {
-	double n, f, val;
-	[val->f],n->f,(java.lang.Exception){fact};
-	f = val - 1;
-	n->f;
+	    double n, f, arg, val;
+	    [arg->val],n->f,(java.lang.Exception){fact};
+	    val = arg - 1;
+	    n->f;
     }@*/
 
 	double fact( Subtask s, double n ) throws Exception {
 
-		if( n == 0 ) {
-			return 1;
-		} 
-		Object[] in = new Object[]{ n };
-   		//in[0] = n - 1;
-    		Object[] out = s.run(in);
-	
-		double f = n * fact( s, ( (Double) out[0]).doubleValue() );
-	
-		System.out.println( "n: " + n + " f: " + f );
-		return f;
-	}
-	
+        if ( n == 0 ) {
+            return 1;
+        }
+        Object[] in = new Object[] { n };
+        Object[] out = s.run( in );
+
+        double f = n * fact( s, ( (Double) out[ 0 ] ).doubleValue() );
+
+        System.out.println( "n: " + n + " f: " + f );
+        return f;
+    }
+
 }
+
+
 
 
 
